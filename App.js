85d36658ce1123e6be71aa4cfb8351e10a68b3ad
Atte Huhtakangas <atte.huhtakangas@gmail.com>
2016-09-18 17:41:43 +0300

    improvements to lunch tab
    * fab is hidden when in lunch tab
    * hide unnecessary shite from the web page
    * add exception for the website for iOS insecure loads

--- App.js
+++ App.js
@@ -5,7 +5,9 @@ import {
   View,
   StatusBar,
   ListView,
+  WebView,
   Text,
+  Dimensions,
 } from 'react-native';
 import {
   Container,
@@ -30,6 +32,7 @@ import Theme from './Theme';
 
 const TODAY_REF = 'TODAY';
 const WEEK_REF = 'WEEK';
+const LUNCH_REF = 'LUNCH';
 
 type Props = {
 };
@@ -37,10 +40,12 @@ type Props = {
 type State = {
   // lessons: Array<Lesson>;
   loading: boolean;
+  fabVisible: boolean;
 };
 
 class App extends Component<*, Props, State> {
   props: Props;
+  onTabChange: Function;
 
   constructor(props: Props) {
     super(props);
@@ -48,12 +53,28 @@ class App extends Component<*, Props, State> {
     this.state = {
       // lessons: [],
       loading: true,
+      fabVisible: true,
     };
+
+    this.onTabChange = this.onTabChange.bind(this);
   }
 
   state: State;
 
+  onTabChange(value: Object) {
+    if (value.ref.ref === LUNCH_REF) {
+      this.setState({
+        fabVisible: false,
+      });
+    } else if (!this.state.fabVisible) {
+      this.setState({
+        fabVisible: true,
+      });
+    }
+  }
+
   render(): React.Element<*> {
+    const fabVisible = this.state.fabVisible;
     return (
       <View style={{ flex: 1 }}>
         <StatusBar
@@ -67,9 +88,9 @@ class App extends Component<*, Props, State> {
             </Button> */}
             <Title>TAMK lukkari</Title>
           </Header>
-          <Content>
+          <View style={{ flex: 0, flexDirection: 'column' }}>
             <Tabs
-              onChangeTab={(val) => console.log(val.i, val.ref.ref)}
+              onChangeTab={this.onTabChange}
               // tabBarPosition="overlayTop"
               // style={{ marginTop: 56 }}
               // tabBarUnderlineStyle={{ height: 4,}}
@@ -77,31 +98,70 @@ class App extends Component<*, Props, State> {
             >
               <Today ref={TODAY_REF} tabLabel="Tänään" />
               <Week ref={WEEK_REF} tabLabel="Viikko" />
+              <WebView
+                tabLabel="Lounaslista"
+                ref={LUNCH_REF}
+                source={{ uri: 'http://www.campusravita.fi/ruokalista' }}
+                style={{ flex: 1 }}
+                automaticallyAdjustContentInsets={true}
+                startInLoadingState={true}
+                renderLoading={() =>
+                  <View style={{ flex: 1 }}>
+                    <Spinner color="rgb(224, 36, 93)" />
+                  </View>
+                }
+                javaScriptEnabled={true}
+                injectedJavaScript={
+                  `(function hideElements() {
+                    var asides = document.getElementsByTagName('aside');
+                    Array.prototype.forEach.call(asides, function(aside) {
+                      aside.innerHTML = '';
+                    });
+                    var header = document.getElementsByClassName('container header')[0];
+                    header.innerHTML = '';
+                    header.style.padding = 0;
+                    document.getElementsByClassName('navbar-header')[0].innerHTML = '';
+                    document.getElementById('navbar').style.minHeight = 0;
+                    var css = '.main-container > .row { padding: 30px 0 40px 0; }';
+                    var head = document.head || document.getElementsByTagName('head')[0];
+                    var style = document.createElement('style');
+                    style.type = 'text/css';
+                    if (style.styleSheet) {
+                      style.styleSheet.cssText = css;
+                    } else {
+                      style.appendChild(document.createTextNode(css));
+                    }
+                    head.appendChild(style);
+                  }());`
+                }
+              />
             </Tabs>
-          </Content>
+          </View>
         </Container>
-        <ActionButton
-          buttonColor="rgba(255, 62, 128, 1)"
-          bgColor="rgba(238, 238, 238, 0.59)"
-          offsetX={16}
-          offsetY={0}
-          style={styles.fab}
-        >
-          <ActionButton.Item
-            buttonColor="#9b59b6"
-            title="Lisää kurssi"
-            onPress={() => console.log("notes tapped!")}
+        {fabVisible &&
+          <ActionButton
+            buttonColor="rgba(255, 62, 128, 1)"
+            bgColor="rgba(238, 238, 238, 0.59)"
+            offsetX={16}
+            offsetY={0}
+            style={styles.fab}
           >
-            <Icon name="md-add" style={styles.actionButtonIcon} />
-          </ActionButton.Item>
-          <ActionButton.Item
-            buttonColor="#1abc9c"
-            title="Vaihda ryhmää"
-            onPress={() => {}}
-          >
-            <Icon name="md-swap" style={styles.actionButtonIcon} />
-          </ActionButton.Item>
-        </ActionButton>
+            <ActionButton.Item
+              buttonColor="#9b59b6"
+              title="Lisää kurssi"
+              onPress={() => {}}
+            >
+              <Icon name="md-add" style={styles.actionButtonIcon} />
+            </ActionButton.Item>
+            <ActionButton.Item
+              buttonColor="#1abc9c"
+              title="Vaihda ryhmää"
+              onPress={() => {}}
+            >
+              <Icon name="md-swap" style={styles.actionButtonIcon} />
+            </ActionButton.Item>
+          </ActionButton>
+        }
       </View>
     );
   }
